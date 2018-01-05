e9a0cf98b09291ea704b5a5f5bcaddbf412fe413
David Garcia <david.a.g.r93@gmail.com>
2018-01-05 14:04:00 -0400

    modified permissions of LLocationManager

--- PokeRadar/ViewController.swift
+++ PokeRadar/ViewController.swift
@@ -9,18 +9,38 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+    
+    var geoFire: GeoFire!
+    let locationManager = CLLocationManager()
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.mapView.delegate = self
+        self.mapView.userTrackingMode = .follow
+        self.locationManager.delegate = self
+        locationAuthStatus()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            self.mapView.showsUserLocation = true
+        } else {
+            self.locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            self.mapView.showsUserLocation = true
+        }
     }
 
     @IBAction func reportPokemon(_ sender: UIButton) {
