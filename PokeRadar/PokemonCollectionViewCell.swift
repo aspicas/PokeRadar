d6b5d9ec29786d882605ec624d03ab08e77f0e43
David Garcia <david.a.g.r93@gmail.com>
2018-01-08 10:32:47 -0400

    Configure pokedex

---
+++ PokeRadar/PokemonCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonCollectionViewCell.swift
+//  PokeRadar
+//
+//  Created by David Garcia on 1/8/18.
+//  Copyright © 2018 David Garcia. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var pokemonImageView: UIImageView!
+    @IBOutlet weak var pokemonLabel: UILabel!
+    @IBOutlet weak var pokemonIdLabel: UILabel!
+    
+}
